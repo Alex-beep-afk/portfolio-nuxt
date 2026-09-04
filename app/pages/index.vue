6d078a4e054742ebce081239ff41d949235c583c
Alexandre_maison <alexandre.prigent2910@gmail.com>
2026-09-04 16:46:42 +0200

    fix(projects): video src resolution by vite on index and project page (case sensitive)

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -36,7 +36,7 @@ const { arrivedState } = useScroll(typeof window !== 'undefined' ? window : null
   <main class="min-h-screen bg-gradient-to-r from-black from-[20%] via-black via-[50%] to-transparent">
     <video autoplay loop muted playsinline class="absolute w-full h-full object-cover -z-10">
 
-      <source :src="'/medias/backgroundFolio.mp4'" type="video/mp4">
+      <source :src="'/medias/BackgroundFolio.mp4'" type="video/mp4">
     </video>
 
 
--- app/pages/projects.vue
+++ app/pages/projects.vue
@@ -9,7 +9,7 @@ await fetchProjects();
 <template>
     <main class="relative min-h-screen">
         <video autoplay loop muted playsinline class="absolute inset-0 w-full h-full object-cover -z-10 ">
-            <source :src="'/medias/backgroundFolio.mp4'" type="video/mp4">
+            <source :src="'/medias/BackgroundFolio.mp4'" type="video/mp4">
         </video>
         <section class="pt-32 pb-8 px-8 lg:mt-8 lg:px-16 ">
             <SectionGridProjects :projects="projects" />
